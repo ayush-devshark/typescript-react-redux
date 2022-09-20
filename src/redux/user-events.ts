567e96e0ce6826bf70c238f6701b1914ec599a26
ayush-devshark <ayushagarwal.dev@gmail.com>
2022-09-20 22:01:11 +0530

    add redux-thunk and action creators for redux-thunk

--- src/redux/user-events.ts
+++ src/redux/user-events.ts
@@ -1,4 +1,6 @@
-import { AnyAction } from 'redux';
+import { RootState } from './store';
+import { Action, AnyAction } from 'redux';
+import { ThunkAction } from 'redux-thunk';
 
 interface UserEvent {
   id: number;
@@ -18,11 +20,57 @@ const initialState: UserEventsState = {
   allIds: [],
 };
 
+const LOAD_REQUEST = 'userEvents/load_request';
+interface LoadRequestAction extends Action<typeof LOAD_REQUEST> {}
+
+const LOAD_SUCCESS = 'userEvents/load_success';
+
+interface LoadSuccessAction extends Action<typeof LOAD_SUCCESS> {
+  payload: {
+    events: UserEvent[];
+  };
+}
+
+const LOAD_FAILURE = 'userEvents/load_failure';
+
+interface LoadFailureAction extends Action<typeof LOAD_FAILURE> {
+  error: string;
+}
+
+export const loadUserEvents =
+  (): ThunkAction<
+    void,
+    RootState,
+    undefined,
+    LoadRequestAction | LoadSuccessAction | LoadFailureAction
+  > =>
+  async (dispatch, getState) => {
+    dispatch({ type: LOAD_REQUEST });
+    try {
+      const response = await fetch('http://localhost:3001/events');
+      const events: UserEvent[] = await response.json();
+
+      dispatch({ type: LOAD_SUCCESS, payload: { events } });
+    } catch (err) {
+      dispatch({ type: LOAD_FAILURE, error: 'Failed to load events.' });
+    }
+  };
+
 const userEventReducer = (
   state: UserEventsState = initialState,
-  action: AnyAction
+  action: LoadSuccessAction
 ) => {
   switch (action.type) {
+    case LOAD_SUCCESS:
+      const { events } = action.payload;
+      return {
+        ...state,
+        allIds: events.map(({ id }) => id),
+        byIds: events.reduce<UserEventsState['byIds']>((byIds, event) => {
+          byIds[event.id] = event;
+          return byIds;
+        }, {}),
+      };
     default:
       return state;
   }
